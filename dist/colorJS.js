5d3bdf982d5cab15c8e2e8dfdd4e8246059df837
SwiftBanshee <38956081+SwiftBanshee@users.noreply.github.com>
2020-09-28 19:06:35 -0400

    first push

---
+++ dist/colorJS.js
@@ -0,0 +1,52 @@
+ 
+        var colorSheets = [
+            {
+                color: "#bb915b",
+                title: "Switch to Default",
+                href:  "dist/color-default.css"
+            },
+            {
+                color: "#d21515",
+                title: "Switch to style-1",
+                href:  "css/color-style/style-1.css"
+            },
+            {
+                color: "#0891d7",
+                title: "Switch to style-2",
+                href:  "css/color-style/style-2.css"
+            },
+           {
+                color: "#343db9",
+                title: "Switch to style-3",
+                href:  "css/color-style/style-3.css"
+            },
+			{
+                color: "#e5c51a",
+                title: "Switch to style-4",
+                href:  "css/color-style/style-4.css"
+            },
+			
+			{
+                color: "#d27b15",
+                title: "Switch to style-5",
+                href:  "css/color-style/style-6.css" 
+            },
+			{
+                color: "#d814d2",
+                title: "Switch to style-6",
+                href:  "css/color-style/style-7.css"
+            },
+			{
+                color: "#36b119",
+                title: "Switch to style-7",
+                href:  "css/color-style/style-8.css"
+            }
+        ];
+        ColorSwitcher.init(colorSheets);
+    
+	
+	
+	
+	
+	
+	
---
+++ js/colorJS.js
@@ -0,0 +1,58 @@
+ 
+        var colorSheets = [
+            {
+                color: "#bb915b",
+                title: "Switch to atlanta_logo",
+                href:  "css/atlanta_logo.css"
+            },
+
+            {
+                color: "#d21515",
+                title: "Switch to style-1",
+                href:  "css/red.css"
+            },
+            {
+                color: "#0891d7",
+                title: "Switch to blue",
+                href:  "css/blue.css"
+            },
+            {
+                color: "#343db9",
+                title: "Switch to nvblue",
+                href:  "css/nvblue.css"
+            },
+           {
+                color: "#e5c51a",
+                title: "Switch to yellow",
+                href:  "css/yellow.css"
+            },
+			{
+                color: "#bb915b",
+                title: "Switch to brown",
+                href:  "css/brown.css"
+            },
+			{
+                color: "#d27b15",
+                title: "Switch to orange",
+                href:  "css/orange.css" 
+            },
+			{
+                color: "#d814d2",
+                title: "Switch to pink",
+                href:  "css/pink.css"
+            },
+			{
+                color: "#36b119",
+                title: "Switch to green",
+                href:  "css/green.css"
+            },
+			
+        ];
+
+    
+	
+	
+	
+	
+	
+	
